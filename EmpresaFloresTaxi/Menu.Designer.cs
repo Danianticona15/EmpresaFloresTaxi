e1cf3b52f5b0c34bbd64eccf3798ba345590f1d9
Danianticona15 <117753940+Danianticona15@users.noreply.github.com>
2024-06-04 15:35:26 -0600

    MENU

---
+++ EmpresaFloresTaxi/Menu.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace EmpresaFloresTaxi
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.iNICIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mANTENIMIENTOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aYUDAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTROCLIENTEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTROSECRETARIAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTROCONDUCTORToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTRORESERVACIONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTROCLIENTEToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTRARSECRETARIAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTRARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEGISTRARRESERVACIONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iNICIOToolStripMenuItem,
+            this.mANTENIMIENTOToolStripMenuItem,
+            this.aYUDAToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(414, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // iNICIOToolStripMenuItem
+            // 
+            this.iNICIOToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rEGISTROCLIENTEToolStripMenuItem,
+            this.rEGISTROSECRETARIAToolStripMenuItem,
+            this.rEGISTROCONDUCTORToolStripMenuItem,
+            this.rEGISTRORESERVACIONToolStripMenuItem});
+            this.iNICIOToolStripMenuItem.Name = "iNICIOToolStripMenuItem";
+            this.iNICIOToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.iNICIOToolStripMenuItem.Text = "INICIO";
+            // 
+            // mANTENIMIENTOToolStripMenuItem
+            // 
+            this.mANTENIMIENTOToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rEGISTROCLIENTEToolStripMenuItem1,
+            this.rEGISTRARSECRETARIAToolStripMenuItem,
+            this.rEGISTRARToolStripMenuItem,
+            this.rEGISTRARRESERVACIONToolStripMenuItem});
+            this.mANTENIMIENTOToolStripMenuItem.Name = "mANTENIMIENTOToolStripMenuItem";
+            this.mANTENIMIENTOToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.mANTENIMIENTOToolStripMenuItem.Text = "REGISTROS";
+            // 
+            // aYUDAToolStripMenuItem
+            // 
+            this.aYUDAToolStripMenuItem.Name = "aYUDAToolStripMenuItem";
+            this.aYUDAToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.aYUDAToolStripMenuItem.Text = "AYUDA";
+            // 
+            // rEGISTROCLIENTEToolStripMenuItem
+            // 
+            this.rEGISTROCLIENTEToolStripMenuItem.Name = "rEGISTROCLIENTEToolStripMenuItem";
+            this.rEGISTROCLIENTEToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTROCLIENTEToolStripMenuItem.Text = "CONSULTA CLIENTE";
+            // 
+            // rEGISTROSECRETARIAToolStripMenuItem
+            // 
+            this.rEGISTROSECRETARIAToolStripMenuItem.Name = "rEGISTROSECRETARIAToolStripMenuItem";
+            this.rEGISTROSECRETARIAToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTROSECRETARIAToolStripMenuItem.Text = "CONSULTA SECRETARIA";
+            // 
+            // rEGISTROCONDUCTORToolStripMenuItem
+            // 
+            this.rEGISTROCONDUCTORToolStripMenuItem.Name = "rEGISTROCONDUCTORToolStripMenuItem";
+            this.rEGISTROCONDUCTORToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTROCONDUCTORToolStripMenuItem.Text = "CONSULTA CONDUCTOR ";
+            // 
+            // rEGISTRORESERVACIONToolStripMenuItem
+            // 
+            this.rEGISTRORESERVACIONToolStripMenuItem.Name = "rEGISTRORESERVACIONToolStripMenuItem";
+            this.rEGISTRORESERVACIONToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTRORESERVACIONToolStripMenuItem.Text = "CONSULTA RESERVACION ";
+            // 
+            // rEGISTROCLIENTEToolStripMenuItem1
+            // 
+            this.rEGISTROCLIENTEToolStripMenuItem1.Name = "rEGISTROCLIENTEToolStripMenuItem1";
+            this.rEGISTROCLIENTEToolStripMenuItem1.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTROCLIENTEToolStripMenuItem1.Text = "REGISTRAR CLIENTE";
+            // 
+            // rEGISTRARSECRETARIAToolStripMenuItem
+            // 
+            this.rEGISTRARSECRETARIAToolStripMenuItem.Name = "rEGISTRARSECRETARIAToolStripMenuItem";
+            this.rEGISTRARSECRETARIAToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTRARSECRETARIAToolStripMenuItem.Text = "REGISTRAR SECRETARIA ";
+            // 
+            // rEGISTRARToolStripMenuItem
+            // 
+            this.rEGISTRARToolStripMenuItem.Name = "rEGISTRARToolStripMenuItem";
+            this.rEGISTRARToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTRARToolStripMenuItem.Text = "REGISTRAR CONDUCTOR ";
+            // 
+            // rEGISTRARRESERVACIONToolStripMenuItem
+            // 
+            this.rEGISTRARRESERVACIONToolStripMenuItem.Name = "rEGISTRARRESERVACIONToolStripMenuItem";
+            this.rEGISTRARRESERVACIONToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.rEGISTRARRESERVACIONToolStripMenuItem.Text = "REGISTRAR RESERVACION ";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 298);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem iNICIOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTROCLIENTEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTROSECRETARIAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTROCONDUCTORToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTRORESERVACIONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mANTENIMIENTOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aYUDAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTROCLIENTEToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTRARSECRETARIAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTRARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEGISTRARRESERVACIONToolStripMenuItem;
+    }
+}
