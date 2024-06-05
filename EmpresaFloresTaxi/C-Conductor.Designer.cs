408feb690564ff037f51c419399b2c23c9dc643b
Daniela Isabel Anticona Custodio <N00241870@upn.pe>
2024-06-04 19:59:57 -0500

    cambio-nombre

--- EmpresaFloresTaxi/C-Conductor.Designer.cs
+++ EmpresaFloresTaxi/C-Conductor.Designer.cs
@@ -219,7 +219,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(142, 411);
+            this.button2.Location = new System.Drawing.Point(145, 411);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 29;
