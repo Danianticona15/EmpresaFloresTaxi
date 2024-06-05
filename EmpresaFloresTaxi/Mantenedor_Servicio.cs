6a935e78a2e0f1f5d39bbf13f88d29b30a7facec
Bruno Aaron Julca Alayo <N00363100@upn.pe>
2024-06-04 20:10:46 -0500

    señito ya lo hice

---
+++ EmpresaFloresTaxi/Mantenedor_Servicio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmpresaFloresTaxi
+{
+    public partial class Mantenedor_Servicio : Form
+    {
+        public Mantenedor_Servicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
